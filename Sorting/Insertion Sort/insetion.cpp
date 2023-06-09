b4a31e4f687dbee92dee2e359512a581fc939aab
YASHASVI PARASHAR <yashasviparashar28@gmail.com>
2023-06-09 18:29:02 +0530

    Insertion Sort

--- Sorting/Insertion Sort/insetion.cpp
+++ Sorting/Insertion Sort/insetion.cpp
@@ -1,34 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void insertion(int *arr,int size)
+void insertion(int *arr, int size)
 {
-    for(int i=1;i<size;i++)
+    for (int i = 1; i < size; i++)
     {
-        int temp=arr[i];
-        int j=i-1;
-        while(j>=0)
+        int temp = arr[i];
+        int j = i - 1;
+        while (j >= 0)
         {
-            if(temp<arr[j])
+            if (temp < arr[j])
             {
-                arr[j+1]=arr[j];
-                
+                arr[j + 1] = arr[j];
             }
-            else{
+            else
+            {
                 break;
             }
             j--;
-            arr[j+1]=temp;
+            arr[j + 1] = temp;
         }
     }
 }
 int main()
 {
-    int arr[]={3,2,4,1};
-    int size=4;
-    insertion(arr,size);
-    // for(int i=0;i<size;i++)
-    // {
-    //     cout<<arr[i]<<" ";
-    // }
-
+    int arr[] = {3, 2, 4, 1};
+    int size = 4;
+    insertion(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
