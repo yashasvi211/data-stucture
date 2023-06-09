26cf7c2e5985d5501f7aa569a37cd9f429bc0ddb
YASHASVI PARASHAR <yashasviparashar28@gmail.com>
2023-06-09 17:25:47 +0530

    Update Insertion

---
+++ Sorting/Insertion Sort/insetion.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+void insertion(int *arr,int size)
+{
+    for(int i=1;i<size;i++)
+    {
+        int temp=arr[i];
+        int j=i-1;
+        while(j>=0)
+        {
+            if(temp<arr[j])
+            {
+                arr[j+1]=arr[j];
+                
+            }
+            else{
+                break;
+            }
+            j--;
+            arr[j+1]=temp;
+        }
+    }
+}
+int main()
+{
+    int arr[]={3,2,4,1};
+    int size=4;
+    insertion(arr,size);
+    // for(int i=0;i<size;i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+
+}
