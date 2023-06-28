8c341b8a8db2e77fbba6a45a10fc86bb5f8404c8
YASHASVI PARASHAR <yashasviparashar28@gmail.com>
2023-06-28 10:08:28 +0530

    permutation

--- Strings/permutation_string.cc
+++ Strings/permutation_string.cc
@@ -9,21 +9,5 @@ int main(void)
     cout<<"Enter the permutation string:";
     getline(cin,s2);
     
-    int arr[26]={0};
-    for(int i=0;i<s1.size();i++)
-    {
-        int h=s1[i]-'a';
-        arr[h]++;
- 
-    }
-      for(int i=0;i<26;i++)
-    {
-        if(arr[i]==0)
-        {
-            continue;
-        }
-      cout<<arr[i]<<" ";
-     
-    }
- 
+
 }
