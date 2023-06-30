15d3de02e5f89ee144fef99c2e8065ea053851a9
YASHASVI PARASHAR <yashasviparashar28@gmail.com>
2023-06-30 18:40:40 +0530

    2d array

---
+++ 2D Arrays/intro.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <array>
+using namespace std;
+int main(void)
+{
+    //creting 2d array
+    int arr[3][3];
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
