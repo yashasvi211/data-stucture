0a6fb8a95a6bfa13ad6e5a21f55e1248c82ee0df
YASHASVI PARASHAR <yashasviparashar28@gmail.com>
2023-07-06 23:04:21 +0530

    update

--- Questions/Arrays/best_time_to_buy_sell_stock.cpp
+++ Questions/Arrays/best_time_to_buy_sell_stock.cpp
@@ -10,22 +10,10 @@ public:
         cout<<maxProfit(prices);
         
     }
-    int maxProfit(vector<int>& prices) 
-    {
-        int n = prices.size();
-        int maxi = 0;
-        int minPrice = numeric_limits<int>::max();
-        
-        for (int i = 0; i < n; i++) {
-            minPrice = min(minPrice, prices[i]);
-            maxi = max(maxi, prices[i] - minPrice);
-        }
-        
-        return maxi;
-    }
+    
 };
 int main()
 {
-    Solution a;
+    Solution a; 
     a.test();
 }
