6035d84fb3a0e43021176d04ac393fa5c50ef082
YASHASVI PARASHAR <yashasviparashar28@gmail.com>
2023-07-10 23:18:34 +0530

    Update rotate_image_2_best.cpp

--- Arrays II/rotate_image_2_best.cpp
+++ Arrays II/rotate_image_2_best.cpp
@@ -29,7 +29,7 @@ int main() {
         }
         cout << endl;
     }
-    cout << "Rotated Image" << endl;
+    cout << "Rotated Image:" << endl;
     for (int i = 0; i < arr.size(); i++) {
     for (int j = 0; j < arr[0].size(); j++) {
         cout << arr[i][j] << " ";
