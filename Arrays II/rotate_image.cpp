013ed86416bef5c1da61f1a47a77cb537dfe3f6e
YASHASVI PARASHAR <yashasviparashar28@gmail.com>
2023-07-10 23:02:42 +0530

    Update rotate_image.cpp

--- Arrays II/rotate_image.cpp
+++ Arrays II/rotate_image.cpp
@@ -1,29 +1,41 @@
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
-vector < vector < int >> rotate(vector < vector < int >> & matrix) {
+vector<vector<int>> rotate(vector<vector<int>> &matrix)
+{
     int n = matrix.size();
-    vector < vector < int >> rotated(n, vector < int > (n, 0));
-   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n;j++)  {
-         rotated[j][i-n-1] = matrix[i][j];
-    }
+    vector<vector<int>> rotated(n, vector<int>(n, 0));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            rotated[j][i - n - 1] = matrix[i][j];
+        }
     }
     return rotated;
 }
 
-int main() {
-    vector < vector < int >> arr;
-    arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    cout<<"Initial Array:";
-    vector < vector < int >> rotated = rotate(arr);
+int main()
+{
+    vector<vector<int>> arr;
+    arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    cout << "Initial Array:" << endl;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr[0].size(); j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+    }
+    vector<vector<int>> rotated = rotate(arr);
     cout << "Rotated Image" << endl;
-    for (int i = 0; i < rotated.size(); i++) {
-    for (int j = 0; j < rotated[0].size(); j++) {
-        cout << rotated[i][j] << " ";
+    for (int i = 0; i < rotated.size(); i++)
+    {
+        for (int j = 0; j < rotated[0].size(); j++)
+        {
+            cout << rotated[i][j] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-    }
-
 }
