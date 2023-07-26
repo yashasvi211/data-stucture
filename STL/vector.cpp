a50c9bd3a7c310917506ae87194e4432b435d806
Yashasvi <yashasviparashar28@gmail.com>
2023-07-26 22:15:50 +0530

    update vector

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main()
 {
-    cout<<"Hello World";
+    vector<int>arr;
+    int i=0;
+    cout<<"Enter a number to push in vector:";
+    cin>>i;
+    for(int j=0;j<i;j++)
+    {
+        int data;
+        cout<<"Enter data for index "<<j<<":";
+        arr.push_back(data);
+    }
+        for(int j=0;j<i;j++)
+    {
+        int data;
+        cout<<"Data of index "<<j<<":"<<arr[j];
+ 
+    }
+    cout<<"Arr back"<<arr.back()<<endl;
+    cout<<"arr capicity"<<arr.capacity()<<endl;
+    cout<<"arr empty"<<arr.empty()<<endl;
+    
 }
