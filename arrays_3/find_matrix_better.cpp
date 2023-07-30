50ad7124f7a9243f216f0a8463b154fd456375c5
Yashasvi <yashasviparashar28@gmail.com>
2023-07-30 22:41:26 +0530

    update find_matrix_better

--- arrays_3/find_matrix_better.cpp
+++ arrays_3/find_matrix_better.cpp
@@ -2,50 +2,37 @@
 #include <vector>
 using namespace std;
 
-void binary_search(vector<vector<int>> arr, int target)
-{
-    int row=arr.size();
-    int col=arr[0].size();
- 
-    int i=row-1;
-    int j=0;
-    while(j<col and i>=0)
-    {
-        if(arr[i][j]==target)
-        {
-            cout<<"The element is at:"<<arr[i][j];
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int col = matrix[0].size();
+    int row = matrix.size();
+    int j = 0;
+    int i = row - 1;
+    while (i >= 0 && j < col) {
+        if (matrix[i][j] == target) {
+            cout << "The element is at: " << i << "," << j << endl;
+            return true;
+        } else if (matrix[i][j] > target) {
+            i--;
+        } else {
+            j++;
         }
-        
     }
-    
-
+    return false;
 }
-int main()
-{
-    /*
-    Condition:
-    Only applicable if matrix if row wise and col wise sorted like:
-    e.g:
-    11 20 31 32
-    15 29 39 50
-    16 30 40 80
-                                or
-    Also applicable if matix is fully sorted like:
-    e.g:
-    11 20 31 32
-    35 40 45 50
-    55 60 61 65
-    Note:If this is the condition we can apply full binary sort which is benificial.
 
-    */
-    vector<vector<int>> arr =
-        {
-            {43, 12, 43},
-            {421, 11, 65},
-            {54, 23, 10}};
-    auto target = 12;
-
-   
+int main() {
+    vector<vector<int>> matrix =
+    {
+        {12,13, 24},
+        {14,20,60},
+        {54,83,90}
+    };
+    int target = 83;
+    if (searchMatrix(matrix, target)) {
+        cout << "Target element found!" << endl;
+    } else {
+        cout << "Target element not found!" << endl;
+    }
 
     return 0;
 }
